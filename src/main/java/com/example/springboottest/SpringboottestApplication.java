58bd2eff3c05c74b702f03ff657ac2610003613a
Corbin Taylor <cptaylor38@yahoo.com>
2021-01-17 15:13:25 -0500

    testing new route

--- src/main/java/com/example/springboottest/SpringboottestApplication.java
+++ src/main/java/com/example/springboottest/SpringboottestApplication.java
@@ -4,19 +4,36 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
 
+
 public class SpringboottestApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(SpringboottestApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringboottestApplication.class, args);
 	}
 
+	
 	@RequestMapping(value = "/")
 	public String hello() {
 		return "Hello World";
+	}
+
+	@RequestMapping(value = "/test")
+	public String test(){
+		return "test";
 	}
 
 }
